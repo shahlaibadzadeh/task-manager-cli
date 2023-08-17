4810e1d4ab554752ddb3992301409deba0fa5fdb
Shahla <shahlaibadzadeh@gmail.com>
2023-08-17 20:59:30 +0400

    add(task_manager): updating tasks function added

--- db.py
+++ db.py
@@ -3,10 +3,9 @@ import sqlite3
 
 task_db = sqlite3.connect('tasks.db')
 cursor = task_db.cursor()
-print("db created")
-# cursor.execute("DROP TABLE tasks")
-try:
 
+try:
+    # Creation of table
     task_db.execute(f'''create table tasks
                     (id integer primary key autoincrement,
                     name text not null,
@@ -33,10 +32,39 @@ except:
             cursor.execute('''insert into tasks(name, description, deadline, created_at, updated_at, is_done) values
                 (?, ?, ?, ?, ?, ?)''', (self.name, self.description, self.deadline, self.created_at, self.updated_at, self.is_done)
                            )
-            print(self.created_at)
             task_db.commit()
-            print("inserted")
+            print("Task added successfully!")
             # task_db.close()
 
-      
+        def list_names_of_tasks():
+            cursor.execute('select * from tasks')
+            rows = cursor.fetchall()
+            for row in rows:
+                print(f'{row[0]}. {row[1]}')
 
+        def update_task(task_id, info_to_update, new_data):
+
+            if info_to_update == "1":
+                cursor.execute(
+                    'Update tasks set name = ? where id = ?', (new_data, task_id))
+                task_db.commit()
+                print("Task updated successfully")
+            elif info_to_update == "2":
+                cursor.execute(
+                    'Update tasks set description = ? where id = ?', (new_data, task_id))
+                task_db.commit()
+                print("Task updated successfully")
+            elif info_to_update == "3":
+                cursor.execute(
+                    'Update tasks set deadline = ? where id = ?', (new_data, task_id))
+                task_db.commit()
+                print("Task updated successfully")
+            elif info_to_update == "4":
+                cursor.execute(
+                    'Update tasks set is_done = ? where id = ?', (new_data, task_id))
+                task_db.commit()
+                print("Task updated successfully")
+            else:
+                print("Wrong choice")
+
+        
--- task_manager.py
+++ task_manager.py
@@ -1,4 +1,4 @@
-import db
+from db import Db_manager
 import datetime
 
 if __name__ == "__main__":
@@ -15,9 +15,29 @@ if __name__ == "__main__":
             task_created_at = datetime.datetime.now()
             task_updated_at = datetime.datetime.now()
             is_done = input("Enter the status of task (0 or 1): ")
-            db.Db_manager(task_name, task_description, task_deadline,
+            Db_manager(task_name, task_description, task_deadline,
                           task_created_at, task_updated_at, is_done).add_task()
+            
             if input("Add another task? Y/N: ").lower() == "n":
                 break
 
-  
+    elif user_input == "2":
+        print("Which task do you want to update? ")
+        Db_manager.list_names_of_tasks() 
+        task_id_input = int(input("Enter your option: "))
+        print("Which information do you want to update? ", "1. Name",
+              "2. Description", "3. Deadline", "4. Task Status", sep="\n")
+        info_to_update = input("Enter your option: ")
+        new_data_input = input("Enter new information: ")
+        Db_manager.update_task(task_id_input, info_to_update, new_data_input)
+
+    elif user_input == "3":
+        print("Which task do you want to delete? ")
+        Db_manager.list_names_of_tasks() 
+        deleted_task_id_input = int(input("Enter your option: "))
+        Db_manager.delete_task(deleted_task_id_input)
+
+    elif user_input == "4":
+        Db_manager.list_tasks()
+
+
